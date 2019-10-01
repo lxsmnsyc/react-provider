874def8490dbd501122d32b701b94605778c8588
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-01 18:23:54 +0800

    Update PromiseProvider.tsx

--- src/Promise/PromiseProvider.tsx
+++ src/Promise/PromiseProvider.tsx
@@ -1,36 +1,99 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
 import * as React from 'react';
-
-interface PromiseProvider<T> {
+import Provider from '../Provider';
+interface PromiseProviderProps<T> {
     value: Promise<T>,
     children: React.ReactNode,
-}
+};
 
-interface PromiseLoading {
-    loading: true,
-    value: null,
-    error: null,
-}
+export type PromiseStates = 'default' | 'loading' | 'success' | 'failure';
 
-interface PromiseSuccess<T> {
-    loading: false,
+export type PromiseDefault = {
+    state: 'default',
+    error?: null,
+    value?: null,
+};
+
+export type PromiseLoading = {
+    state: 'loading',
+    error?: null,
+    value?: null,
+};
+
+export type PromiseSuccess<T> = {
+    state: 'success',
     value: T,
-    error: null,
-}
+    error?: null,
+};
 
-interface PromiseFailure {
-    loading: false,
-    value: null,
+export type PromiseFailure = {
+    state: 'failure',
     error: Error,
-}
+    value?: null,
+};
 
-type PromiseResult<T> = PromiseLoading | PromiseSuccess<T> | PromiseFailure;
+export type PromiseResult<T> = PromiseDefault | PromiseLoading | PromiseSuccess<T> | PromiseFailure;
 
-function usePromise<T>(value: Promise<T>) {
+function usePromise<T>(promise: Promise<T>) {
+    const [state, setState] = React.useState<PromiseResult<T>>({
+        state: 'default',
+    });
 
+    const mounted = React.useRef(false);
+
+    /**
+     * Component lifecycle tracking
+     */
+    React.useEffect(() => {
+        mounted.current = true;
+        return () => {
+            mounted.current = false;
+        };
+    }, []);
+
+    React.useEffect(() => {
+        setState({ state: 'loading' });
+
+        promise.then(
+            value => mounted.current && setState({ state: 'success', value }),
+            error => mounted.current && setState({ state: 'failure', error }),
+        );
+    }, [ promise ]);
+
+    return state;
 }
 
 export default function PromiseProvider<T>({ value, children }: PromiseProviderProps<T>) {
+    const state = usePromise(value);
     return (
-        
-    )
-}
+        <Provider value={state}>
+            { children }
+        </Provider>
+    );
+};
