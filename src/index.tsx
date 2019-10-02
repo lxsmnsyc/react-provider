8557aaa4ca2b785a995c904f9d69225f7a48b0b3
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 13:02:31 +0800

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export { default as Provider } from './Provider';
+export { default as Consumer } from './Consumer';
+export { default as Selector } from './Selector';
+export { useProvider } from './useProvider';
