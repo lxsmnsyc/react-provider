408845044fef6aba9843288c907033eea1acc0c5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 14:52:25 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,12 @@ export { default as Provider } from './Provider';
 export { default as Consumer } from './Consumer';
 export { default as Selector } from './Selector';
 export { useProvider } from './useProvider';
+
+export { default as PromiseProvider } from './Promise/PromiseProvider';
+export { default as PromiseConsumer } from './Promise/PromiseConsumer';
+export { usePromiseProvider } from './Promise/usePromiseProvider';
+
+export { default as ChangeNotifier } from './ChangeNotifier/ChangeNotifier';
+export { default as ChangeNotifierProvider } from './ChangeNotifier/ChangeNotifierProvider';
+export { default as ChangeNotifierConsumer } from './ChangeNotifier/ChangeNotifierConsumer';
+export { useChangeNotifierProvider } from './ChangeNotifier/useChangeNotifierProvider';
