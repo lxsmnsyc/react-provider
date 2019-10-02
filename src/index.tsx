6b7349832ca45f658bc8407cf2fb0f0949b27bb1
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 15:37:49 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-export { default as Provider } from './Provider';
-export { default as Consumer } from './Consumer';
-export { default as Selector } from './Selector';
+export { Provider } from './Provider';
+export { Consumer } from './Consumer';
+export { Selector } from './Selector';
 export { useProvider } from './useProvider';
 
-export { default as PromiseProvider } from './Promise/PromiseProvider';
-export { default as PromiseConsumer } from './Promise/PromiseConsumer';
+export { PromiseProvider } from './Promise/PromiseProvider';
+export { PromiseConsumer } from './Promise/PromiseConsumer';
 export { usePromiseProvider } from './Promise/usePromiseProvider';
 
-export { default as ChangeNotifier } from './ChangeNotifier/ChangeNotifier';
-export { default as ChangeNotifierProvider } from './ChangeNotifier/ChangeNotifierProvider';
-export { default as ChangeNotifierConsumer } from './ChangeNotifier/ChangeNotifierConsumer';
+export { ChangeNotifier } from './ChangeNotifier/ChangeNotifier';
+export { ChangeNotifierProvider } from './ChangeNotifier/ChangeNotifierProvider';
+export { ChangeNotifierConsumer } from './ChangeNotifier/ChangeNotifierConsumer';
 export { useChangeNotifierProvider } from './ChangeNotifier/useChangeNotifierProvider';
+
+export { MultiProvider } from './MultiProvider';
