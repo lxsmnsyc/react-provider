46023667a74528b157fd76967b6380065b63e9d8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 16:31:14 +0800

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
 
-import { MultiProvider, Consumer, Provider } from 'react-provider'
+import { Provider, Consumer } from 'react-provider'
 
 export default function App() {
   return (
-    <MultiProvider
-      providers={[
-        <Provider of="A" value="Hello World" />,
-        <Provider of="B" value="Hello Alexis" />,
-        <Provider of="C" value="Hello Lyon" />,
-      ]}
-    >
+    <Provider of="A" value="Hello World">
       <Consumer of="A" builder={x => <h1>{x}</h1>} />
-      <Consumer of="B" builder={x => <h1>{x}</h1>} />
-      <Consumer of="C" builder={x => <h1>{x}</h1>} />
-    </MultiProvider>
+    </Provider>
   )
 }
