c9dd55736fc16dae0b31e351d700fb542c6d6d65
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 13:02:26 +0800

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import ExampleComponent from 'react-provider'
+import { Provider, Consumer } from 'react-provider'
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <Provider value="Hello World">
+      
+    </Provider>
+  )
 }
