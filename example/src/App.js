97d0b76aeeb177abbf39841ca7dd3ac2ffb1a48c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-02 14:51:39 +0800

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
-import { Provider, Consumer } from 'react-provider'
+import { useChangeNotifierProvider, ChangeNotifierProvider, ChangeNotifierConsumer, ChangeNotifier } from 'react-provider'
+
+class CounterModel extends ChangeNotifier {
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  increment() {
+    this.count++;
+    this.notifyListeners();
+  }
+
+  decrement() {
+    this.count--;
+    this.notifyListeners();
+  }
+}
+
+function Increment() {
+  const notifier = useChangeNotifierProvider('Test', false);
+  const onClick = useCallback(() => notifier.increment(), [ notifier ]);
+  return (
+    <button type="button" onClick={onClick}>Increment!</button>
+  )
+}
+
+function Decrement() {
+  const notifier = useChangeNotifierProvider('Test', false);
+  const onClick = useCallback(() => notifier.decrement(), [ notifier ]);
+  return (
+    <button type="button" onClick={onClick}>Decrement!</button>
+  )
+}
 
 export default function App() {
   return (
-    <Provider value="Hello World">
-      
-    </Provider>
+    <ChangeNotifierProvider of="Test" value={new CounterModel()}>
+      <ChangeNotifierConsumer
+        of="Test"
+        builder={model => <h1>{model.count}</h1>}
+      />
+      <Increment />
+      <Decrement />
+    </ChangeNotifierProvider>
   )
 }
