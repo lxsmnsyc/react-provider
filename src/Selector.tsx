70c508098475cb38c68f9b705bde124626f3ffcc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-10-04 17:37:15 +0800

    Update Selector.tsx

--- src/Selector.tsx
+++ src/Selector.tsx
@@ -27,35 +27,48 @@
  */
 import * as React from 'react';
 import { ProviderFinder, useProvider } from './useProvider';
-import { BiFunction, Function } from './utils/Function';
+import { Function, Function2, Function3, Function4 } from './utils/Function';
 import { Optional } from './utils/Optional';
-
-export type SelectorFunction<T, R> = Function<T, R>;
-
 /**
  * A builder function type for the Selector.builder property
  */
-export type SelectorBuilder<T> = BiFunction<T, Optional<React.ReactNode>, React.ReactElement>;
-
-/**
- * Prop type definition for the SelectorComponent (Selector.component)
- */
-export interface ISelectorComponentProps<T> {
-  value: T,
-  children?: React.ReactNode,
-}
-
-/**
- * Type definition for the SelectorComponent (Selector.component)
- */
-export type SelectorComponent<T> = React.ComponentType<ISelectorComponentProps<T>>;
-
+export type SelectorBuilder<T> = Function2<T, Optional<React.ReactNode>, React.ReactElement>;
 /**
  * Prop type definition for the Selector component
  */
 export interface ISelectorProps<T, R> {
   of: ProviderFinder<T>,
-  selector: SelectorFunction<T, R>,
+  selector: Function<T, R>,
+  children?: React.ReactNode,
+  builder: SelectorBuilder<R>,
+}
+export interface ISelector2Props<T1, T2, R> {
+  of: [
+    ProviderFinder<T1>,
+    ProviderFinder<T2>,
+  ],
+  selector: Function2<T1, T2, R>,
+  children?: React.ReactNode,
+  builder: SelectorBuilder<R>,
+}
+export interface ISelector3Props<T1, T2, T3, R> {
+  of: [
+    ProviderFinder<T1>,
+    ProviderFinder<T2>,
+    ProviderFinder<T3>,
+  ],
+  selector: Function3<T1, T2, T3, R>,
+  children?: React.ReactNode,
+  builder: SelectorBuilder<R>,
+}
+export interface ISelector4Props<T1, T2, T3, T4, R> {
+  of: [
+    ProviderFinder<T1>,
+    ProviderFinder<T2>,
+    ProviderFinder<T3>,
+    ProviderFinder<T4>,
+  ],
+  selector: Function4<T1, T2, T3, T4, R>,
   children?: React.ReactNode,
   builder: SelectorBuilder<R>,
 }
@@ -77,22 +90,38 @@ export function Selector<T, R>({ of, selector, builder, children }: ISelectorPro
   /**
    * Consume value from the Provider
    */
-  const value = useProvider<Optional<T>>(of);
-
-  /**
-   * Check if the value does not exist
-   */
-  if (value == null) {
-    return null;
-  }
+  const value = useProvider<T>(of);
   /**
    * Perform value selection
    */
   const selected = selector(value);
-
   /**
    * Memoize the result of the builder to prevent re-build calls,
    * where the selected value is a dependency value.
    */
+  return React.useMemo(() => builder(selected, children), [ selected ]);
+}
+
+export function Selector2<T1, T2, R>({ of, selector, builder, children }: ISelector2Props<T1, T2, R>) {
+  const v1 = useProvider<T1>(of[0]);
+  const v2 = useProvider<T2>(of[1]);
+  const selected = selector(v1, v2);
+  return React.useMemo(() => builder(selected, children), [ selected ]);
+}
+
+export function Selector3<T1, T2, T3, R>({ of, selector, builder, children }: ISelector3Props<T1, T2, T3, R>) {
+  const v1 = useProvider<T1>(of[0]);
+  const v2 = useProvider<T2>(of[1]);
+  const v3 = useProvider<T3>(of[2]);
+  const selected = selector(v1, v2, v3);
+  return React.useMemo(() => builder(selected, children), [ selected ]);
+}
+
+export function Selector4<T1, T2, T3, T4, R>({ of, selector, builder, children }: ISelector4Props<T1, T2, T3, T4, R>) {
+  const v1 = useProvider<T1>(of[0]);
+  const v2 = useProvider<T2>(of[1]);
+  const v3 = useProvider<T3>(of[2]);
+  const v4 = useProvider<T4>(of[3]);
+  const selected = selector(v1, v2, v3, v4);
   return React.useMemo(() => builder(selected, children), [ selected ]);
 }
