aa7da0ad9c722884d431e78c462a6a89230cf754
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-04 14:05:43 +0800

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -2,15 +2,23 @@ import React, { useCallback } from 'react'
 
 import { useChangeNotifierProvider, ChangeNotifierProvider, ChangeNotifierConsumer, ChangeNotifier } from 'react-provider'
 
+const sleep = time => new Promise((res) => setTimeout(res, time, true));
 class CounterModel extends ChangeNotifier {
   constructor() {
     super();
     this.count = 0;
+    this.loading = false;
   }
 
   increment() {
-    this.count++;
+    this.loading = true;
     this.notifyListeners();
+    sleep(500).then(() => {
+      this.count++;
+      this.loading = false;
+      this.notifyListeners();
+
+    });
   }
 
   decrement() {
@@ -20,10 +28,12 @@ class CounterModel extends ChangeNotifier {
 }
 
 function Increment() {
-  const notifier = useChangeNotifierProvider('Test', false);
+  const notifier = useChangeNotifierProvider('Test', true);
   const onClick = useCallback(() => notifier.increment(), [ notifier ]);
   return (
-    <button type="button" onClick={onClick}>Increment!</button>
+    <button type="button" onClick={onClick} disabled={notifier.loading}>
+      { notifier.loading ? 'Loading' : 'Increment!'}
+    </button>
   )
 }
 
